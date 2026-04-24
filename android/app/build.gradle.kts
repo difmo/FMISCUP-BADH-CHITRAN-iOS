59f32454385f78020d246e4157e1144e1de7365a
difmo <difmotech@gmail.com>
2026-04-24 11:44:09 +0530

    save

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.1.4"
+        versionCode = 18
+        versionName = "1.1.8"
     }
     signingConfigs {
         create("release") {
